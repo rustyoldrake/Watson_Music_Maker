45721626e4c7b2bcaf63541ad491f8f3cc9f794a
Ryan Anderson <rustyoldrake@gmail.com>
2016-10-17 17:36:44 -0700

    Add files via upload

---
+++ keys.R
@@ -0,0 +1,36 @@
+# keys.R
+# PRIVATE FILE DO NOT SHARE
+# http://blog.revolutionanalytics.com/2015/11/how-to-store-and-use-authentication-details-with-r.html
+
+# CONVERSATION - 
+username_CONV <- "XXXXXXX-a8ce-4ab5-XXXX-ee218a203cce"
+password_CONV <- "XXXXXXXX"
+username_password_CONV = paste(username_CONV,":",password_CONV,sep="")
+workspace_CONV <- "XXXXXXXX-6701-XXXX-bcc1-0d3519d0b944"
+
+# TONE ANALYZER  - AUTHENTICATION AND CREDENTIALS 
+username_TON = "XXXXXX-82cb-4862-XXXX-8f4fcf55326f"
+password_TON = "XXXXXXXX"
+username_password_TON = paste(username_TON,":",password_TON)
+
+# NLC CREDENTIALS - AUTHENTICATION AND CREDENTIALS 
+username_NLC = "XXXXXXX-6f1f-4d2c-XXXX-54f54038667e"
+password_NLC = "XXXXXXX"
+username_password_NLC = paste(username_NLC,":",password_NLC,sep="")
+
+
+# Alchemy
+username_password_ALCH <- "XXXXXXXXXf30440aaa160dd1da4d8126c6b285"
+
+# STT - Speech-To-Text credentials on Bluemix
+username_STT <-"XXXXXXXXX-b5bb-406c-XXXX-7cb964fb614f"
+password_STT <- "XXXXXXXXXX"  
+username_password_STT = paste(username_STT,":",password_STT,sep="")
+
+# TTS - TEXT TO SPEECH - AUTHENTICATION AND CREDENTIALS 
+username_TTS <-"XXXXXXXX-48c4-486e-XXXX-174388b22b17" 
+password_TTS <- "XXXXXXXXXX"  
+username_password_TTS = paste(username_TTS,":",password_TTS,sep="")
+
+
+
